952375f362de2a1cd6e329dba73843c25d43d023
brotato <lamtung.truong@gmail.com>
2019-12-16 18:36:02 +0100

    - stylechecks missing java doc -.- zum 2.

--- src/test/java/de/hda/fbi/db2/test/AllTests.java
+++ src/test/java/de/hda/fbi/db2/test/AllTests.java
@@ -3,14 +3,15 @@ package de.hda.fbi.db2.test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * Test-Suite.
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
         {Lab01Test.class,
                 Lab02Test.class,
                 Lab03Test.class}
 )
-/**
- * Test-Suite.
- */
+
 public class AllTests {
 }
